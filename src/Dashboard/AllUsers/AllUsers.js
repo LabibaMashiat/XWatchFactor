d7d30a83c0157c99d19bd2b6d2eeb35a9b72faee
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2022-11-30 20:55:28 +0600

    delete user by admin

--- src/Dashboard/AllUsers/AllUsers.js
+++ src/Dashboard/AllUsers/AllUsers.js
@@ -1,7 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
+import ConfirmationModal from '../../Pages/Shared/ConfirmationModal/ConfirmationModal';
 
 const AllUsers = () => {
+    const[deletingUser,setDeletingUser]=useState(null);
+    const closeModal = () => {
+        setDeletingUser(null);
+      };
     const { data: users = [], refetch } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
@@ -12,8 +18,26 @@ const AllUsers = () => {
           return data;
         },
       });
+      const handleDeleteUser=user=>{
+        fetch(
+            `http://localhost:5000/users/${user._id}`,
+            {
+              method: "DELETE",
+              
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+              if (data.deletedCount > 0) {
+                refetch();
+                toast.success("User deleted successfully");
+              }
+            });
+    }
     return (
-        <div className="">
+       <div>
+         <div className="">
         <table className="table w-full">
           <thead>
             <tr>
@@ -23,24 +47,44 @@ const AllUsers = () => {
               <th>User Phone</th>
               <th>User Location</th>
               <th>Status</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
           
                 {
-                    users.map(user=><tr>
-                        <th>1</th>
+                    users.map((user,i)=><tr key={user._id}>
+                        <th>{i+1}</th>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>{user.phone}</td>
                     <td>{user.location}</td>
                     <td>{user.status}</td>
+                    <td>
+                  <label
+                    onClick={() => setDeletingUser(user)}
+                    htmlFor="confirmation-modal"
+                    className="btn bg-orange-400 btn-sm btn-error"
+                  >
+                    Delete
+                  </label>
+                </td>
                     </tr>)
                 }
            
           </tbody>
         </table>
       </div>
+       {deletingUser && (
+        <ConfirmationModal
+          title={`Are you sure you want to delete?`}
+          successAction={handleDeleteUser}
+          successButtonName="Delete"
+          modalData={deletingUser}
+          closeModal={closeModal}
+        ></ConfirmationModal>
+      )}
+       </div>
     );
 };
 
